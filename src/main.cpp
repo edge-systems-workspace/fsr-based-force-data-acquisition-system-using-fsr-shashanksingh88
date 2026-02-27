9de8ccf3120ea066e41bfd29e3a0ccfc4fb156d6
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-27 17:29:37 +0530

    Define FSR analog pin (Use A0)

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,50 @@
  * displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define FSR analog pin (Use A0)
+// TODO 1:
+// Define FSR analog pin (Use A0)
+#define FSR_PIN A0
 
- // TODO 2:
- // Create variable to store sensor reading
+// TODO 2:
+// Create variable to store sensor reading
+//int fsrValue = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+  //  Serial.begin(9600);
 
     // TODO 4:
     // Print system initialization message
+    //Serial.println("FSR Force Measurement System");
+    //Serial.println("Initializing...");
+    delay(2000);
 }
 
 void loop() {
 
     // TODO 5:
     // Read analog value from FSR
+    //fsrValue = analogRead(FSR_PIN);
 
     // TODO 6:
     // Print raw ADC value
+    //Serial.print("Raw ADC Value: ");
+    //Serial.println(fsrValue);
 
     // TODO 7:
     // Apply simple threshold logic (e.g., detect pressure)
+    //if (fsrValue > 200) {
 
-    // TODO 8:
-    // Print pressure detection message
+        // TODO 8:
+        // Print pressure detection message
+      //  Serial.println("Pressure Detected!");
+    } else {
+        //Serial.println("No Significant Pressure.");
+    }
 
     // TODO 9:
     // Add delay (500ms or 1 second)
+    delay(1000);
 }
